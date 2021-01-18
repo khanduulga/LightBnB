7316149cebebb8248cb2c502c7faba0f1875aa2b
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2021-01-18 18:25:54 +0000

    Implemented getallreservations query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -76,7 +76,16 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `SELECT reservations.*, properties.*
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < NOW()::date
+  ORDER BY start_date
+  LIMIT $2;`
+  const queryParams = [`${guest_id}`, limit]
+  return pool.query(queryString, queryParams)
+  .then(res => res.rows);
 }
 exports.getAllReservations = getAllReservations;
 
